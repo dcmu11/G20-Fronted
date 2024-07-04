f9d2479371ccab990adc0f41ad1e65ed1c4bea0d
dcmu11 <dcmum11@gmail.com>
2024-07-03 22:54:59 -0500

    js1

---
+++ semana 6/dia1/main.js
@@ -0,0 +1,36 @@
+//iniciando js
+
+
+//numerico como se declara una variable
+// var numero=15 no se usa var por que puede crear conficto
+
+//number
+let numero1=prompt("ingrea numero 1");
+let numero2=prompt("ingrea numero 2");
+const numero3=30;
+let  suma=parseInt(numero1)+parseInt(numero2);
+
+console.log("la suma es ",suma);
+
+
+//string
+//let alumno=prompt("ingresa Nombre");
+
+
+//alert("hola ",alumno);
+
+//booleano
+let bool=true;
+let bool2=0;
+
+//mull-- representa datos no declarados
+let nada=null;
+
+//undefine
+// juan
+
+// // ventana que guarda datos
+// prompt("ingresa datos");
+
+// //venrana emergente
+// alert("hola");
